3029b0c436f09ada780de51d6a01240f1c317121
dacoit <dacoit@tuta.io>
2015-08-08 19:12:06 -0500

    reposition xsession

--- X11/xinitrc.d/00-xsession.sh
+++ X11/xinitrc.d/00-xsession.sh
@@ -1,3 +1,10 @@
-[[ -f "$XDG_CONFIG_HOME"/X11/xsession ]] && {
-    source "$XDG_CONFIG_HOME"/X11/xsession
-}
+#!/bin/bash
+#XDG_CONFIG_HOME/X11/xsession
+
+[[ -f $XDG_CONFIG_HOME/X11/Xresources ]] && xrdb "$XDG_CONFIG_HOME"/X11/Xresources
+
+xss-lock -- lock &
+setxkbmap -option compose:caps
+dunst -conf "$XDG_CONFIG_HOME"/dunst/dunstrc &
+redshift -c "$XDG_CONFIG_HOME"/redshift/redshift.conf &
+feh --no-fehbg --randomize --recursive --bg-scale "$XDG_DATA_HOME"/wallpapers/*
