87ec01db7dbe7ab440f7602b060c682a79f70428
dacoit <dacoit@tuta.io>
2015-08-08 19:06:28 -0500

    fix quoting

--- X11/xinitrc.d/99-exec-wm.sh
+++ X11/xinitrc.d/99-exec-wm.sh
@@ -1,6 +1,6 @@
 case "$WM" in 
     i3|dwm|bspwm|xmonad)
-        exec "$WM" > "$XDG_CACHE_HOME/$WM-runtime.log" 2>&1
+        exec "$WM" > "$XDG_CACHE_HOME"/"$WM"-runtime.log 2>&1
         ;;
     *)
         "$WM"
